5f4d176bd56b91a8eedc2c724f0155e502a180b0
linhampton <prc-lh138-17@PRC-LH138-17s-iMac.local>
2016-07-11 14:29:44 -0500

    Big2Joke

---
+++ Contents.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Who's there?"
+
+let reply = "Knock, Knock"
+
+reply
+
+str
+
+let answer = "apple"
+
+let answer2 = "orange"
+
+let answer3 = "orange who?"
+
+let answer4 = "Orange ya glad I did not freaking say APPLE?"
+
+
+print (reply, terminator:"")
+
+print (str, terminator:"")
+
+print (answer, terminator:"")
+
+print (reply, terminator:"")
+
+print (str, terminator:"")
+
+print (answer, terminator:"")
+
+print (reply, terminator:"")
+
+print (str, terminator:"")
+
+print (answer, terminator:"")
+
+print (reply, terminator:"")
+
+print (str, terminator:"")
+
+print (answer2, terminator:"")
+
+print (answer3, terminator:"")
+
+print (answer4, terminator:"")
+
+
+
+
+
+
+
